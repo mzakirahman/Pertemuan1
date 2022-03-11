ef552663b546ebdba4345d3cc1e7600620ec831c
ZakiRahman <mzakiriau@gmail.com>
2022-03-11 16:53:35 +0700

    percobaan array of character

--- src/main/kotlin/13-EksplisidPadaChar.kt
+++ src/main/kotlin/13-EksplisidPadaChar.kt
@@ -11,12 +11,4 @@ fun main() {
     c = i.toChar()
     println("The ASCII value of $i was converted to its textual value of $c")
 
-
-    // bertipe boolean
-    var varBool : Boolean = true
-    if(varBool)
-        println("varBool bernilai " + varBool)
-    var varInt : Int = 1
-    if(varInt)
-        println("varInt bernilai " + varInt)
 }
---
+++ src/main/kotlin/14-ArrayOfCharacter.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val myName: String = "Muhammad Zaki R"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
