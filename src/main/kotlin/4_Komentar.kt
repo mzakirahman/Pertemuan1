25b1d671d0e94248f60b5f5ae7be3b6659502016
ZakiRahman <mzakiriau@gmail.com>
2022-03-11 16:15:03 +0700

    percobaan Komentar

---
+++ src/main/kotlin/4_Komentar.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    // komen dalam 1 baris
+    /*
+    lebih dari 1 baris
+     */
+    /**
+     * Kotlin baris ini untuk mendokumentasikan kode program
+     */
+}
