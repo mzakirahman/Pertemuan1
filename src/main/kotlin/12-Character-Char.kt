ccb58169c3b6d48a3bc35279c6c1fe1319d3be5f
ZakiRahman <mzakiriau@gmail.com>
2022-03-11 16:49:03 +0700

    percobaan Explisid Pada Char

--- src/main/kotlin/12-Character-Char.kt
+++ src/main/kotlin/12-Character-Char.kt
@@ -1,4 +1,10 @@
 fun main() {
     var charA : Char = 'A'
-    println(charA == 65)
+    //println(charA == 65)
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.toLowerCase())
+    val strA: String = charA.toString()
+    println("Kini charA sudah menjadi String = "+strA)
 }
